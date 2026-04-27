54895b1b0a89e7323ac59771b380727176987a81
Abdulrhman mohamed salama <154723658+abdulrhmansalama@users.noreply.github.com>
2026-04-27 10:41:15 +0300

    Refactor DDA and midpoint line algorithms

--- lines.py
+++ lines.py
@@ -1,16 +1,29 @@
-def dda(x0, y0, x1, y1):
+def dda_line(x0, y0, x1, y1):
     points = []
     dx = x1 - x0
     dy = y1 - y0
-    steps = max(abs(dx), abs(dy))
-    x_inc = dx / steps
-    y_inc = dy / steps
+    # حساب الميل
+    m = dy / dx
     x = x0
     y = y0
-    for _ in range(steps + 1):
+    points.append((round(x), round(y)))
+    # نكرر لحد ما نوصل للنقطة النهائية
+    while x < x1 or y < y1:
+        # الحالة 1: m < 1
+        if m < 1:
+            x_new = x + 1
+            y_new = y + m
+        # الحالة 2: m > 1
+        elif m > 1:
+            y_new = y + 1
+            x_new = x + (1 / m)
+        # الحالة 3: m = 1
+        else:
+            x_new = x + 1
+            y_new = y + 1
+        x = x_new
+        y = y_new
         points.append((round(x), round(y)))
-        x += x_inc
-        y += y_inc
     return points
 
 
@@ -33,17 +46,26 @@ def bresenham(x0, y0, x1, y1):
 
 def midpoint_line(x0, y0, x1, y1):
     points = []
+    # Step 01: حساب ΔX و ΔY
     dx = x1 - x0
     dy = y1 - y0
-    d = 2 * dy - dx
-    x = x0
-    y = y0
-    while x <= x1:
-        points.append((x, y))
-        if d < 0:
-            d = d + 2 * dy
+    # Step 02: حساب Dinitial و ΔD
+    D = 2 * dy - dx
+    delta_D = 2 * (dy - dx)
+    xk = x0
+    yk = y0
+    points.append((xk, yk))
+    # Step 03 + Step 04
+    for _ in range(dx):
+        # Case 01: D < 0
+        if D < 0:
+            xk = xk + 1
+            yk = yk
+            D = D + 2 * dy
+        # Case 02: D >= 0
         else:
-            y += 1
-            d = d + 2 * (dy - dx)
-        x += 1
+            xk = xk + 1
+            yk = yk + 1
+            D = D + delta_D
+        points.append((xk, yk))
     return points
